fcee2d0712e222c6e918349670dd333d1e455bcd
Gabriel Rea <gabriel.rea96@gmail.com>
2022-05-22 23:01:35 -0300

    exercise-case-0 completed

--- list-search/case-0/src/App.tsx
+++ list-search/case-0/src/App.tsx
@@ -9,22 +9,26 @@ function App() {
   const [query, setQuery] = useState<string>("");
 
   useEffect(() => {
-    api.search(query);
+    api.search(query.toLowerCase()).then((res) => setProducts(res));
   }, [query]);
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h4>{product.title}</h4>
-            <p>{product.description}</p>
-            <span>$ {product.price}</span>
-          </li>
-        ))}
-      </ul>
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+      {products.length > 0 ? (
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              <h4>{product.title}</h4>
+              <p>{product.description}</p>
+              <span className={product.price <= 100 ? "sale" : ""}>$ {product.price}</span>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Cargando....</p>
+      )}
     </main>
   );
 }
